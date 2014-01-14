0648156726db7adfa3eb4efc442e95ec79af2a04
Nguyen Tran Trung <nguyentrantrung1986@gmail.com>
2014-01-14 15:49:17 +0700

    add sample module to Nuke viet 4.0

---
+++ nukeviet-develop/modules/samples/theme.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * @Project NUKEVIET 4.x
+ * @Author VINADES.,JSC (contact@vinades.vn)
+ * @Copyright (C) 2014 VINADES.,JSC. All rights reserved
+ * @License GNU/GPL version 2 or any later version
+ * @Createdate Tue, 14 Jan 2014 08:23:14 GMT
+ */
+
+if ( ! defined( 'NV_IS_MOD_SAMPLES' ) ) die( 'Stop!!!' );
+
+/**
+ * nv_theme_samples_main()
+ * 
+ * @param mixed $array_data
+ * @return
+ */
+function nv_theme_samples_main ( $array_data )
+{
+    global $global_config, $module_name, $module_file, $lang_module, $module_config, $module_info, $op;
+
+    $xtpl = new XTemplate( $op . '.tpl', NV_ROOTDIR . '/themes/' . $module_info['template'] . '/modules/' . $module_file );
+    $xtpl->assign( 'LANG', $lang_module );
+
+    
+
+    $xtpl->parse( 'main' );
+    return $xtpl->text( 'main' );
+}
+
+/**
+ * nv_theme_samples_detail()
+ * 
+ * @param mixed $array_data
+ * @return
+ */
+function nv_theme_samples_detail ( $array_data )
+{
+    global $global_config, $module_name, $module_file, $lang_module, $module_config, $module_info, $op;
+
+    $xtpl = new XTemplate( $op . '.tpl', NV_ROOTDIR . '/themes/' . $module_info['template'] . '/modules/' . $module_file );
+    $xtpl->assign( 'LANG', $lang_module );
+
+    
+
+    $xtpl->parse( 'main' );
+    return $xtpl->text( 'main' );
+}
+
+/**
+ * nv_theme_samples_search()
+ * 
+ * @param mixed $array_data
+ * @return
+ */
+function nv_theme_samples_search ( $array_data )
+{
+    global $global_config, $module_name, $module_file, $lang_module, $module_config, $module_info, $op;
+
+    $xtpl = new XTemplate( $op . '.tpl', NV_ROOTDIR . '/themes/' . $module_info['template'] . '/modules/' . $module_file );
+    $xtpl->assign( 'LANG', $lang_module );
+
+    
+
+    $xtpl->parse( 'main' );
+    return $xtpl->text( 'main' );
+}
+
+?>
---
+++ nukeviet-develop/modules/samples/version.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @Project NUKEVIET 4.x
+ * @Author VINADES.,JSC (contact@vinades.vn)
+ * @Copyright (C) 2014 VINADES.,JSC. All rights reserved
+ * @License GNU/GPL version 2 or any later version
+ * @Createdate Tue, 14 Jan 2014 08:23:14 GMT
+ */
+
+if ( ! defined( 'NV_MAINFILE' ) ) die( 'Stop!!!' );
+
+$module_version = array(
+		'name' => 'Samples',
+		'modfuncs' => 'main,detail,search',
+		'submenu' => 'main,detail,search',
+		'is_sysmod' => 0,
+		'virtual' => 1,
+		'version' => '4.0.0',
+		'date' => 'Tue, 14 Jan 2014 08:23:15 GMT',
+		'author' => 'VINADES (contact@vinades.vn)',
+		'uploads_dir' => array($module_name),
+		'note' => ''
+	);
+
+?>
